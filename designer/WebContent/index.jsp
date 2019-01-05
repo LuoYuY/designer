06dcf0e3d37b6e6bb39d2dd54b75f272aee3dad1
luoyuying <15868808504@163.com>
2019-01-05 13:28:01 +0800

    init designer project

---
+++ designer/WebContent/index.jsp
@@ -0,0 +1,353 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="s" uri="/struts-tags"%>
+<%@ taglib prefix="sx" uri="/struts-dojo-tags"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html lang="en">
+<head>
+<title>首 页</title>
+<!-- custom-theme -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords"
+	content="Upholstery Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript">
+	
+	 addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+		function hideURLbar(){ window.scrollTo(0,1); } 
+
+</script>
+<!-- //custom-theme -->
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css"
+	media="all" />
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<link rel="stylesheet" href="css/flexslider.css" type="text/css"
+	media="screen" property="" />
+<!-- js -->
+<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+<!-- //js -->
+<!-- font-awesome-icons -->
+<link rel="stylesheet" href="css/font-awesome.min.css" />
+<!-- //font-awesome-icons -->
+<link
+	href="//fonts.googleapis.com/css?family=Josefin+Sans:100,100i,300,300i,400,400i,600,600i,700,700i"
+	rel="stylesheet">
+<link
+	href="//fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&amp;subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext,vietnamese"
+	rel="stylesheet">
+</head>
+
+<body>
+	<!-- banner -->
+	<div class="banner jarallax">
+		<div class="agile_dot_info one">
+			<div class="container">
+				<nav class="navbar navbar-default">
+				<div class="navbar-header navbar-left">
+					<button type="button" class="navbar-toggle collapsed"
+						data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="sr-only">Toggle navigation</span> <span
+							class="icon-bar"></span> <span class="icon-bar"></span> <span
+							class="icon-bar"></span>
+					</button>
+					<h1>
+						<a class="navbar-brand" href="index.html" style="font-size: 30px;">初见室内设计交易平台</a>
+					</h1>
+				</div>
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse navbar-right"
+					id="bs-example-navbar-collapse-1">
+					<nav class="link-effect-8" id="link-effect-8">
+					<ul class="nav navbar-nav">
+						<li class="active"><a href="index.html">首页</a></li>
+						<li><a href="findAllExample">看案例</a></li>
+						<li><a href="findneeds">找需求</a></li>
+						<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">装修攻略 <b class="caret"></b></a>
+							<ul class="dropdown-menu agile_short_dropdown">
+								<li><a href="icons.html">问答</a></li>
+								<li><a href="typography.html">装修宝典</a></li>
+							</ul></li>
+						<s:if test="%{#session.id==null}">
+							<li><a href="login.jsp">登陆</a></li>
+							<li><a href="contact.html">注册</a></li>
+						</s:if>
+						
+						<s:else>
+						
+							
+						  <s:if test="%{#session.designer!=null}">
+						  <li><a href="judge.action" target="_Blank">个人主页</a>
+							</li>
+							<li><a href="designerIndex.jsp">个人中心</a></li>
+						  </s:if>
+						  <s:elseif test="%{#session.employer!=null}">
+						 
+							<li><a href="employerIndex.jsp">个人中心</a></li>
+						  </s:elseif>
+						</s:else>
+						
+						
+						
+							
+							
+					</ul>
+					</nav>
+				</div>
+				</nav>
+
+				<div class="w3_agile_banner_info">
+					<div class="maintitle">
+						<p>发布项目，按需雇佣设计师</p>
+						<p>1小时内对接设计师</p>
+					</div>
+					<div class="mainbutton">
+
+						<s:if test="%{#session.designer!=null}">
+							<a href="findneeds"><input class="button button_border"
+								type="button" value="接受项目" style="margin-top: 4px;"></input></a>
+							<a href="uploadCase.jsp"><input class="button button_shadow"
+								type="button" value="上传案例"></input></a>
+						</s:if>
+						<s:else>
+							<s:if test="%{#session.employer!=null}">
+								<a href="#"><input class="button button_border"
+									type="button" value="发布需求"></input></a>
+								<a href="findall.action"><input class="button button_border"
+									type="button" value="浏览案例"></input></a>
+							</s:if>
+							<s:else>
+								<a href="#"><input class="button button_border"
+									type="button" value="发布需求" style="margin-top: 4px;"></input></a>
+								<a href="#"><input class="button button_shadow"
+									type="button" value="成为设计师"></input></a>
+							</s:else>
+						</s:else>
+					</div>
+				</div>
+
+
+			</div>
+		</div>
+	</div>
+	<!-- //banner -->
+
+	<!-- special -->
+	
+	
+	<!-- team -->
+	<div class="team">
+		<div class="container">
+			<h3 class="w3ls_head">
+				<span>优秀设计师</span>
+			</h3>
+			
+			<div class="agileits_team_grids">
+				<div class="col-md-4 agileits_team_grid">
+					<div class="agileits_team_grid_figure">
+						<img src="images/t1.jpg" alt=" " class="img-responsive" />
+					</div>
+					<h4>Jane Nguyen</h4>
+					<p>General Manager</p>
+					<p>+21 345 287 4556</p>
+					<div class="social-icon">
+						<a href="#" class="social-button facebook"><i
+							class="fa fa-facebook"></i></a> <a href="#"
+							class="social-button twitter"><i class="fa fa-twitter"></i></a> <a
+							href="#" class="social-button google"><i
+							class="fa fa-google-plus"></i></a> <a href="#"
+							class="social-button dribbble"><i class="fa fa-dribbble"></i></a>
+					</div>
+				</div>
+				<div class="col-md-4 agileits_team_grid">
+					<div class="agileits_team_grid_figure">
+						<img src="images/t2.jpg" alt=" " class="img-responsive" />
+					</div>
+					<h4>James Doe</h4>
+					<p>Finance Executive</p>
+					<p>+21 345 287 4556</p>
+					<div class="social-icon">
+						<a href="#" class="social-button facebook"><i
+							class="fa fa-facebook"></i></a> <a href="#"
+							class="social-button twitter"><i class="fa fa-twitter"></i></a> <a
+							href="#" class="social-button google"><i
+							class="fa fa-google-plus"></i></a> <a href="#"
+							class="social-button dribbble"><i class="fa fa-dribbble"></i></a>
+					</div>
+				</div>
+				<div class="col-md-4 agileits_team_grid">
+					<div class="agileits_team_grid_figure">
+						<img src="images/t3.jpg" alt=" " class="img-responsive" />
+					</div>
+					<h4>Laura Carl</h4>
+					<p>Management</p>
+					<p>+21 345 287 4556</p>
+					<div class="social-icon">
+						<a href="#" class="social-button facebook"><i
+							class="fa fa-facebook"></i></a> <a href="#"
+							class="social-button twitter"><i class="fa fa-twitter"></i></a> <a
+							href="#" class="social-button google"><i
+							class="fa fa-google-plus"></i></a> <a href="#"
+							class="social-button dribbble"><i class="fa fa-dribbble"></i></a>
+					</div>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+	</div>
+	<!-- //team -->
+
+	<!-- footer -->
+	<div class="footer">
+		<div class="container">
+			<div class="w3_agile_footer_grids">
+				<div class="col-md-4 w3_agile_footer_grid">
+					<h3>Latest Tweets</h3>
+					<ul class="agile_footer_grid_list">
+						<li><i class="fa fa-twitter" aria-hidden="true"></i>Nam
+							libero tempore, cum soluta nobis est eligendi optio cumque nihil
+							impedit. <span>1 day ago</span></li>
+						<li><i class="fa fa-twitter" aria-hidden="true"></i>Itaque
+							earum rerum hic tenetur a sapiente delectus <a
+							href="mailto:info@mail.com">info@mail.com</a> cumque nihil
+							impedit. <span>2 days ago</span></li>
+					</ul>
+				</div>
+				<div class="col-md-4 w3_agile_footer_grid">
+					<h3>Navigation</h3>
+					<ul class="agileits_w3layouts_footer_grid_list">
+						<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><a
+							href="index.html">Home</a></li>
+						<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><a
+							href="services.html">Services</a></li>
+						<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><a
+							href="projects.html">Projects</a></li>
+						<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><a
+							href="contact.html">Contact</a></li>
+					</ul>
+				</div>
+				<div class="col-md-4 w3_agile_footer_grid">
+					<h3>Latest Works</h3>
+					<div class="w3_agileits_footer_grid_left">
+						<a href="#"><img src="images/s6.jpg" alt=" "
+							class="img-responsive" /></a>
+					</div>
+					<div class="w3_agileits_footer_grid_left">
+						<a href="#"><img src="images/s2.jpg" alt=" "
+							class="img-responsive" /></a>
+					</div>
+					<div class="w3_agileits_footer_grid_left">
+						<a href="#"><img src="images/s1.jpg" alt=" "
+							class="img-responsive" /></a>
+					</div>
+					<div class="w3_agileits_footer_grid_left">
+						<a href="#"><img src="images/s3.jpg" alt=" "
+							class="img-responsive" /></a>
+					</div>
+					<div class="w3_agileits_footer_grid_left">
+						<a href="#"><img src="images/s4.jpg" alt=" "
+							class="img-responsive" /></a>
+					</div>
+					<div class="w3_agileits_footer_grid_left">
+						<a href="#"><img src="images/s5.jpg" alt=" "
+							class="img-responsive" /></a>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<div class="w3_newsletter_footer_grids">
+				<div class="w3_newsletter_footer_grid_left">
+					<form action="#" method="post">
+						<input type="email" name="Email" placeholder="Email" required="">
+						<input type="submit" value="Send">
+					</form>
+				</div>
+
+
+			</div>
+
+			<div class="agileinfo_copyright">
+				<p>
+					© 2018 Upholstery. All Rights Reserved | Design by <a
+						href="https://w3layouts.com/">W3layouts</a>
+				</p>
+			</div>
+		</div>
+	</div>
+	<!-- //footer -->
+	<script type="text/javascript" src="js/move-top.js"></script>
+	<script type="text/javascript" src="js/easing.js"></script>
+	<!-- here stars scrolling icon -->
+	<script type="text/javascript">
+		$(document).ready(function() {
+			/*
+				var defaults = {
+				containerID: 'toTop', // fading element id
+				containerHoverID: 'toTopHover', // fading element hover id
+				scrollSpeed: 1200,
+				easingType: 'linear' 
+				};
+			 */
+
+			$().UItoTop({
+				easingType : 'easeOutQuart'
+			});
+
+		});
+	</script>
+	<!-- //here ends scrolling icon -->
+
+	<!-- for bootstrap working -->
+	<script src="js/bootstrap.js"></script>
+	<!-- //for bootstrap working -->
+	<!-- carousal -->
+	<script src="js/slick.js" type="text/javascript" charset="utf-8"></script>
+	<script type="text/javascript">
+		$(document).on('ready', function() {
+			$(".center").slick({
+				dots : true,
+				infinite : true,
+				centerMode : true,
+				slidesToShow : 2,
+				slidesToScroll : 2,
+				responsive : [ {
+					breakpoint : 768,
+					settings : {
+						arrows : true,
+						centerMode : false,
+						slidesToShow : 2
+					}
+				}, {
+					breakpoint : 480,
+					settings : {
+						arrows : true,
+						centerMode : false,
+						centerPadding : '40px',
+						slidesToShow : 1
+					}
+				} ]
+			});
+		});
+	</script>
+	<!-- //carousal -->
+
+	<script src="js/SmoothScroll.min.js"></script>
+	<script type="text/javascript">
+		/* init Jarallax */
+		$('.jarallax').jarallax({
+			speed : 0.5,
+			imgWidth : 1366,
+			imgHeight : 768
+		})
+	</script>
+	<!-- stats -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<script src="js/jquery.countup.js"></script>
+	<script>
+		$('.counter').countUp();
+	</script>
+	<!-- //stats -->
+
+</body>
+</html>
---
+++ designer/src/cn/edu/zjut/service/DesignerService.java
@@ -0,0 +1,440 @@
+package cn.edu.zjut.service;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import javax.servlet.ServletContext;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.impl.model.jdbc.MySQLBooleanPrefJDBCDataModel;
+import org.apache.mahout.cf.taste.impl.model.jdbc.ReloadFromJDBCDataModel;
+import org.apache.mahout.cf.taste.impl.neighborhood.NearestNUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.TanimotoCoefficientSimilarity;
+import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
+import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+import org.apache.mahout.cf.taste.recommender.Recommender;
+import org.apache.mahout.cf.taste.similarity.UserSimilarity;
+import org.apache.struts2.ServletActionContext;
+import org.hibernate.Transaction;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+import com.opensymphony.xwork2.ActionContext;
+import cn.edu.zjut.dao.DesignerDAO;
+import cn.edu.zjut.dao.ExampleDAO;
+import cn.edu.zjut.dao.IDesignerDAO;
+import cn.edu.zjut.dao.IEmployerDAO;
+import cn.edu.zjut.dao.IExampleDAO;
+import cn.edu.zjut.po.Designer;
+import cn.edu.zjut.po.Employer;
+import cn.edu.zjut.po.Example;
+import cn.edu.zjut.po.ExamplePanorama;
+import cn.edu.zjut.po.ExamplePicture;
+
+public class DesignerService implements IDesignerService {
+
+	private Map<String, Object> request, session;
+	private IDesignerDAO designerDAO = null;
+	private IExampleDAO exampleDAO = null;
+	private IEmployerDAO employerDAO=null;
+	
+	public void setDesignerDAO(IDesignerDAO designerDAO) {this.designerDAO = designerDAO;}
+	public void setExampleDAO(IExampleDAO exampleDAO) {this.exampleDAO = exampleDAO;}
+	public void setEmployerDAO(IEmployerDAO employerDAO) {this.employerDAO = employerDAO;}
+	
+
+	/**涓�у寲鎺ㄨ崘
+	 * Mysql鏁版嵁婧�
+	 * star琛ㄤ腑鏈�30鏉′互涓婃暟鎹」
+	 * **/
+	
+	
+	static private MysqlDataSource dataSource;
+	private List<Example> recommendExamples = new ArrayList<Example>();
+	public DesignerService()   //閰嶇疆鏁版嵁婧�
+	{
+		System.out.println("in service");
+		String driver = "com.mysql.cj.jdbc.Driver";
+	
+		String user = "root";
+		String password = "lyy19971221";
+		try {
+			Class.forName(driver);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		dataSource = new MysqlDataSource();
+		dataSource.setURL("jdbc:mysql://localhost:3306/designer?serverTimezone=GMT%2B8&useUnicode=true&characterEncoding=UTF8");
+		dataSource.setUser(user);
+		dataSource.setPassword(password);
+	}
+	
+	public void recommend(String employerID)
+	{
+		//BasicConfigurator.configure();
+		
+		MySQLBooleanPrefJDBCDataModel datamodel = new MySQLBooleanPrefJDBCDataModel(dataSource, 
+				"star", "collecter", "exampleID",null);
+		//鍒╃敤ReloadFromJDBCDataModel鍖呰９jdbcDataModel,鍙互鎶婅緭鍏ュ姞鍏ュ唴瀛樿绠楋紝鍔犲揩璁＄畻閫熷害銆�
+		ReloadFromJDBCDataModel model;
+		try {
+			model = new ReloadFromJDBCDataModel(datamodel);
+			UserSimilarity similarity =new TanimotoCoefficientSimilarity(model);		//鐢ㄦ埛鐩镐技搴︼紝浣跨敤璋锋湰璺濈鐩镐技搴�
+			UserNeighborhood neighborhood =new NearestNUserNeighborhood(5,similarity,model);    
+			Recommender recommender= new GenericUserBasedRecommender(model,neighborhood,similarity);
+			System.out.println("Long.parseLong(employerID)"+Long.parseLong(employerID));
+			List<RecommendedItem> recommendations =recommender.recommend(Long.parseLong(employerID),4);//涓虹敤鎴�1鎺ㄨ崘涓や釜ItemID
+			System.out.println("recommendations.size():"+recommendations.size());
+			recommendExamples.clear();
+			for(RecommendedItem recommendation :recommendations){
+				System.out.println(recommendation.getItemID());
+				recommendExamples.add(exampleDAO.findById((int)recommendation.getItemID()));
+			}
+			
+		} catch (TasteException e) {
+			// TODO Auto-generated catch block
+			//e.printStackTrace();
+		}
+
+		
+	}
+	
+	
+	
+	public boolean login(Designer desi)
+	{
+		String phone=desi.getPhone();
+		String password=desi.getPassword();
+		ActionContext ctx= ActionContext.getContext();
+		session=(Map)ctx.get("session");
+		request=(Map)ctx.get("request");
+		String hql = "from Designer where phone='" + phone+ "' and password='" + password + "'";
+		String hql2 = "from Employer where phone='" + phone+ "' and password='" + password + "'";
+		List list = designerDAO.findByHql(hql);  //鏌ヨ璁捐甯堣〃
+		List list2 = employerDAO.findByHql(hql2); //鏌ヨ鏅�氱敤鎴疯〃
+		if (!list.isEmpty())
+		{
+			Designer designer=(Designer)list.get(0);
+			session.put("id", designer.getDesignerId());
+			request.put("tip", "鐧诲綍鎴愬姛锛� ");
+			session.put("designer", designer);
+			return true;
+		}
+		else if(!list2.isEmpty())
+		{
+			Employer employer=(Employer)list2.get(0);
+			session.put("id", employer.getEmployerId());
+			session.put("employer", employer);
+			System.out.println("in recommend:");
+			recommend(employer.getEmployerId());
+			session.put("recommendExamples", recommendExamples);
+			System.out.println("in recommend:"+recommendExamples.size());
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+	public boolean upload(Example example, File[] upload, File[] upload2) {
+		// 1.鎷垮埌ServletContext
+		ServletContext servletContext = ServletActionContext.getServletContext();
+		// 鑾峰彇request
+		ActionContext ctx = ActionContext.getContext();
+		request = (Map) ctx.get("request");
+		session = (Map) ctx.get("session");
+		try {
+		    Designer designer = (Designer) session.get("designer");
+			// 2.璋冪敤realPath鏂规硶锛岃幏鍙栨牴鎹竴涓櫄鎷熺洰褰曞緱鍒扮殑鐪熷疄鐩綍
+			String pathOfPanoramas = servletContext
+					.getRealPath("/file/" + designer.getDesignerId() + "/" + example.getName() + "/panoramas/");
+			String pathOfPictures = servletContext
+					.getRealPath("/file/" + designer.getDesignerId() + "/" + example.getName() + "/pictures/");
+			// 3.濡傛灉杩欎釜鐪熷疄鐨勭洰褰曚笉瀛樺湪锛岄渶瑕佸垱寤�
+			File fileOfPanoramas = new File(pathOfPanoramas);
+			File fileOfPictures = new File(pathOfPictures);
+			if (!fileOfPanoramas.exists()) {
+				fileOfPanoramas.mkdirs();
+			}
+			if (!fileOfPictures.exists()) {
+				fileOfPictures.mkdirs();
+			}
+			// 鍓垏锛氭妸涓存椂鏂囦欢鍓垏鎸囧畾鐨勪綅缃紝骞朵笖缁欎粬閲嶅懡鍚嶃�� 娉ㄦ剰锛氫复鏃舵枃浠舵病鏈変簡
+			for (int i = 0; i < upload.length; i++) {
+				upload[i].renameTo(new File(fileOfPanoramas, Integer.toString(i) + ".jpg"));
+				ExamplePanorama panorama = new ExamplePanorama("file/" + designer.getDesignerId() + "/"
+						+ example.getName() + "/panoramas/" + Integer.toString(i) + ".jpg");
+				example.getPanoramas().add(panorama);
+			}
+			for (int i = 0; i < upload2.length; i++) {
+				upload2[i].renameTo(new File(fileOfPictures, Integer.toString(i) + ".jpg"));
+				ExamplePicture picture = new ExamplePicture("file/" + designer.getDesignerId() + "/" + example.getName()
+						+ "/pictures/" + Integer.toString(i) + ".jpg");
+				example.getPictures().add(picture);
+			}
+		    //exampleDAO.save(example);
+			designer.getExamples_own().add(example);
+			designerDAO.update(designer);
+			request.put("tip", "妗堜緥涓婁紶鎴愬姛锛�");
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	
+	
+	public boolean putDesigner(Designer designer) {
+		ActionContext ctx = ActionContext.getContext();
+		session = (Map) ctx.get("session");
+		if (session.get("designer") != null) {
+			String id = ((Designer) session.get("designer")).getDesignerId();
+			if (designer.getDesignerId().equals(id))
+				return true;                             //鏄璁″笀鏈汉
+			else {
+				request = (Map) ctx.get("request");
+				designer = designerDAO.findById(designer.getDesignerId());
+				request.put("designer", designer);
+				return false;                            //鏄叾浠栬璁″笀
+			}
+		} else {
+			request = (Map) ctx.get("request");
+			designer = designerDAO.findById(designer.getDesignerId());
+			request.put("designer", designer);
+			return false;                                  //鏄叾浠栭泧涓�
+		}
+	}
+
+	public boolean registerDes(Designer designer) {
+     	String id=designerDAO.findDes();
+     	if(id==null) {
+     		String a="0"+"000000002";
+     		designer.setDesignerId(a);
+     	}
+     	else {
+     		Integer a=Integer.parseInt(id)+1;
+     		String b=a.toString();
+     		designer.setDesignerId(b);
+     	}
+     	designer.setAccount(designer.getPhone());
+     	designer.setName("abc");
+     	designer.setIDNumber("0000000");
+     	designer.setHmpgbkg("");
+     	designer.setProfilePhoto("");
+     	
+   	    try {
+     	 designerDAO.save(designer);
+     	 return true;
+   	   }catch(Exception e) {
+   		   e.printStackTrace();
+		 }
+		 return false;
+	  }
+
+	public boolean judgeIdentity() {
+		ActionContext ctx = ActionContext.getContext();
+		session = (Map) ctx.get("session");
+		// 鍒ゆ柇鏄璁″笀杩樻槸鐢ㄩ泧涓�
+		if (session.get("designer") != null)// 濡傛灉鏄璁″笀
+			return true;
+		else// 濡傛灉鏄泧涓�
+			return false;
+	}
+	
+	public boolean viewExampleDetails(Designer designer, Example example) {
+		ActionContext ctx = ActionContext.getContext();
+		request = (Map) ctx.get("request");
+		DesignerDAO d_dao = new DesignerDAO();
+		ExampleDAO e_dao = new ExampleDAO();
+
+		example = e_dao.findById(example.getExampleId());
+		designer = d_dao.findById(designer.getDesignerId());
+
+		e_dao.getSession().close();
+		d_dao.getSession().close();
+
+		if (example == null || designer == null)
+			return false;
+		else {
+			request.put("designer", designer);
+			request.put("example", example);
+			return true;
+		}
+	}
+	
+	public boolean findAll() {
+		ActionContext ctx = ActionContext.getContext();
+		String hql = "from cn.edu.zjut.po.Designer";
+		List designers = designerDAO.findByHql(hql);
+		request=(Map)ctx.get("request");
+		request.put("designers", designers);
+		return true;
+	}
+	public boolean findByPraise(){
+		ActionContext ctx = ActionContext.getContext();
+		String hql = "from cn.edu.zjut.po.Designer d where d.praise >=15";
+		List designers = designerDAO.findByHql(hql);
+		request=(Map)ctx.get("request");
+		request.put("designers", designers);
+		return true;
+	}
+	public boolean findByScore(){
+		ActionContext ctx = ActionContext.getContext();
+		String hql = "from cn.edu.zjut.po.Designer d where d.score >=5 ";
+		List designers = designerDAO.findByHql(hql);
+		request=(Map)ctx.get("request");
+		request.put("designers", designers);
+		return true;
+	}
+	public boolean logout() {
+		ActionContext ctx= ActionContext.getContext();
+		session=(Map)ctx.get("session");
+		session.clear();
+		return true;
+	}
+	
+	
+
+	
+	
+	public boolean update(Designer designer, File profile, String profileFileName) {
+		System.out.println("in update");
+		ActionContext ctx= ActionContext.getContext(); 
+		session=(Map) ctx.getSession();
+		request=(Map) ctx.get("request"); 
+		System.out.println("designer.getDesignerId()："+designer.getDesignerId());
+		System.out.println("designer.getAccount()："+designer.getAccount());
+		System.out.println(designer.getPassword());
+		if (profile != null) {
+			String save = "C:\\designer\\designer\\profilephoto";
+			save=copyfile(save,profile,profileFileName);
+			designer.setProfilePhoto(profileFileName);
+			System.out.println(save);
+		}
+		try {
+			designerDAO.update(designer);
+			request.put("tip", "修改个人资料成功");
+			session.put("designer", designer);
+			return true;
+		} catch (Exception e) {
+			request.put("tip", "修改个人资料失败");
+			e.printStackTrace();
+			return false;
+		} 	
+	}
+
+
+	public boolean update2(Designer designer, File certificate, String certificateFileName) {
+		ActionContext ctx= ActionContext.getContext(); 
+		session=(Map) ctx.getSession();
+		request=(Map) ctx.get("request"); 
+		if (certificate != null) {
+			String save2 = "C:\\designer\\designer\\certificate";
+			save2=copyfile(save2,certificate,certificateFileName);
+			designer.setCertificate(certificateFileName);
+			designer.setStatus("待审核");
+			System.out.println(save2);
+		}
+		Designer d=(Designer) session.get("designer");
+		System.out.println(d.getDesignerId());
+		d.setPrize(designer.getPrize());
+		d.setCertificate(designer.getCertificate());
+		d.setStatus(designer.getStatus());
+		try {
+			designerDAO.update(d);
+			request.put("tip", "上传成功，请耐心等待管理员审核");
+			session.put("designer", d);
+			return true;
+		} catch (Exception e) {
+			request.put("tip", "上传失败");
+			e.printStackTrace();
+			return false;
+		} 
+	}   
+
+	//上传图片
+	public String copyfile(String path,File file, String filename) {
+		File f = new File(path);	
+		if (!f.exists())
+			f.mkdirs();
+		try {
+			FileUtils.copyFile(file, new File(f, filename));		
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		path=path+"\\"+filename;
+		return path;	
+	}
+
+
+	public boolean recommend1(int money1) {
+		ActionContext ctx= ActionContext.getContext(); 
+		session=(Map) ctx.getSession();
+		request=(Map) ctx.get("request"); 
+		
+		Designer designer=(Designer) session.get("designer");
+		designer.setStatus1(1);
+		designer.setMoney1(money1);
+
+		try {
+			designerDAO.update(designer);
+			request.put("tip", "申请个人推优成功，请耐心等待管理员批准");
+			session.put("designer", designer);
+			return true;
+		} catch (Exception e) {
+			request.put("tip", "申请个人推优失败");	
+			e.printStackTrace();
+			return false;
+		} 
+	}
+
+
+	public boolean recommend2(int money1) {
+		ActionContext ctx= ActionContext.getContext(); 
+		session=(Map) ctx.getSession();
+		request=(Map) ctx.get("request"); 
+		
+		Designer designer=(Designer)session.get("designer");
+		designer.setStatus1(3);
+		designer.setMoney1(money1);		
+
+		try {
+			designerDAO.update(designer);
+			session.put("designer", designer);
+			return true;
+		} catch (Exception e) {		
+			request.put("tip", "申请案例推优失败");
+			e.printStackTrace();
+			return false;
+		} 
+	}
+
+
+	public boolean recommend3(String message) {
+		ActionContext ctx= ActionContext.getContext(); 
+		session=(Map) ctx.getSession();
+		request=(Map) ctx.get("request"); 
+		
+		Designer designer=(Designer)session.get("designer");
+		designer.setMessage(message);
+		designer.setStatus1(3);
+
+		try {
+			designerDAO.update(designer);
+			session.put("designer", designer);
+			request.put("tip", "申请案例推优成功，请耐心等待管理员通过申请");
+			return true;
+		} catch (Exception e) {
+			request.put("tip", "申请案例推优失败");
+			e.printStackTrace();
+			return false;
+		} 
+	}
+}
